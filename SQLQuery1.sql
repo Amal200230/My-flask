383eb8274133c284cebf64c39357a9a49eb5272e
Amal200230 <abhimal30@gmail.com>
2025-04-10 15:35:36 +0530

    new sql query example

---
+++ SQLQuery1.sql
@@ -0,0 +1,11 @@
+
+select name from sys.databases;
+use amal ;
+
+
+select name from sys.tables;
+
+select * from details;
+update details set place = 'dubai' where name = 'lama';
+
+insert into details (name,place) values('dhoni','chennai')
